08d1415708a940078df321810ea3b6a63605947b
Vinh Phan <pvphan90@gmail.com>
2024-08-07 19:55:20 -0700

    adjusted vis

--- scratch.py
+++ scratch.py
@@ -1,12 +1,15 @@
 import time
 
+import numpy as np
 import rerun as rr
 
 import datasets
 
 
 def main():
-    teapot_dataset = datasets.get_dataset("teapot")
+    teapot_dataset = datasets.get_dataset("teapot", 16_000)
+    teapot_experiment_path = "exps/teapot_experiment/frames.npy"
+    teapot_experiment_data = np.load(teapot_experiment_path)
 
     rr.init("scratch")
     rr.serve(open_browser=False)
@@ -16,6 +19,14 @@ def main():
             positions=teapot_dataset.tensors[0],
         ),
     )
+
+    for data in teapot_experiment_data:
+        rr.log(
+            "points",
+            rr.Points3D(
+                positions=data,
+            ),
+        )
 
     while True:
         time.sleep(1)
